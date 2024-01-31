8dbef2bec6374f40f70f5c48a9262699217c73ce
Dirk Kretschmann <kretschmanndi@gmail.com>
2024-01-31 18:46:15 +0100

    add test positions for experiments

--- src/search/search.rs
+++ src/search/search.rs
@@ -15,12 +15,21 @@ use super::simulate::simulate;
 
 pub fn search(board: Board) {
     //let mut tree = Tree::new(board);
+    //let mut tree = Tree::new(Board::from_fen(
+    //    "rnbqkbnr/1ppppppp/p7/8/8/5N2/PPPPPPPP/RNBQKB1R w KQkq - 0 2",
+    //));
+    //let mut tree = Tree::new(Board::from_fen(
+    //    "rnbqkbnr/ppppp2p/5p2/6p1/8/4P2N/PPPP1PPP/RNBQKB1R w KQkq - 0 3",
+    //));
+    //let mut tree = Tree::new(Board::from_fen(
+    //    "rnbqk2r/2pppp1p/pp5R/8/2n5/4P3/PPPP1PP1/RNBQKB2 w Qkq - 1 9",
+    //));
     let mut tree = Tree::new(Board::from_fen(
-        "rnbqkbnr/1ppppppp/p7/8/8/5N2/PPPPPPPP/RNBQKB1R w KQkq - 0 2",
+        "r4rk1/4n2p/1p2pp2/p1p4P/P3P3/1P2b1P1/8/RN3RK1 w - - 1 19",
     ));
     let mut random = Random::from_seed(111);
 
-    let max = 200000;
+    let max = 5000;
     for i in 0..max {
         let node_index = select(&tree);
         let node_index = expand(&mut tree, node_index);
