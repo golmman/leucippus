ce39cc830c107be1f1dfa2fe11ea703569210709
Dirk Kretschmann <kretschmanndi@gmail.com>
2024-02-11 21:28:43 +0100

    improve usability of bench script

--- bench.sh
+++ bench.sh
@@ -4,20 +4,23 @@ BENCH_COMMAND='cargo run --release -- -m silent -i 1000'
 
 cargo build --release
 
-if [[ $1 == '-q' ]]; then
-    echo 'quick run, no export'
+if [[ $1 != '-r' ]]; then
+    echo "quick run, no export, use '-r' option for release run"
     hyperfine "$BENCH_COMMAND"
     exit 0
 fi
 
-
 CPU_NAME=$(lscpu | grep "Model name:" | sed -r 's/Model name:\s{1,}//g')
 CPU_NAME="${CPU_NAME// /_}"
+EXPORT_DIR_NAME="bench_results/$CPU_NAME"
 EXPORT_FILE_NAME="$(date +%Y%m%d%H%M%S)_$(git rev-parse --short HEAD).json"
+EXPORT_PATH_NAME="$EXPORT_DIR_NAME/$EXPORT_FILE_NAME"
 
-mkdir -p "bench_results/$CPU_NAME"
+echo "release run, export results to '$EXPORT_PATH_NAME'"
+
+mkdir -p "$EXPORT_DIR_NAME"
 
 hyperfine \
     "$BENCH_COMMAND" \
-    --export-json "bench_results/$CPU_NAME/$EXPORT_FILE_NAME" \
+    --export-json "$EXPORT_PATH_NAME" \
     --warmup 1
