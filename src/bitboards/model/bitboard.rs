fa9545d054eb814beee703b897e97f3b63c068e7
Dirk Kretschmann <kretschmanndi@gmail.com>
2024-02-16 00:08:09 +0100

    add bitboard transformations from string and arrays

--- src/bitboards/model/bitboard.rs
+++ src/bitboards/model/bitboard.rs
@@ -30,6 +30,36 @@ impl From<[[u8; 8]; 8]> for Bitboard {
     }
 }
 
+impl From<&str> for Bitboard {
+    fn from(board: &str) -> Self {
+        let mut bb = 0u64;
+        let mut i = 0;
+
+        for ch in board.chars() {
+            if i >= 64 {
+                break;
+            }
+            if ch == '0' {
+                i += 1;
+            } else if ch == '1' {
+                let x = i % 8;
+                let y = i / 8;
+                let j = 8 * (7 - y) + x;
+                bb |= 1u64 << j;
+                i += 1;
+            }
+        }
+
+        Bitboard(bb)
+    }
+}
+
+impl From<String> for Bitboard {
+    fn from(board: String) -> Self {
+        Bitboard::from(board.as_str())
+    }
+}
+
 impl Debug for Bitboard {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut result = String::new();
@@ -56,8 +86,8 @@ mod test {
     use super::*;
 
     #[test]
-    fn it_xx() {
-        let a = [
+    fn it_reads_a_bitboard_from_a_byte_array() {
+        let bb = Bitboard::from([
             [1, 0, 0, 0, 0, 0, 0, 0],
             [1, 1, 0, 0, 0, 0, 0, 0],
             [1, 1, 1, 1, 1, 0, 0, 0],
@@ -66,23 +96,73 @@ mod test {
             [1, 0, 0, 0, 0, 0, 0, 0],
             [1, 0, 0, 0, 0, 0, 0, 0],
             [1, 1, 0, 0, 0, 1, 1, 1],
-        ];
+        ]);
+        let left = format!("{bb:?}");
 
-        let one = Bitboard::from(a);
-        println!("{one:?}");
-        println!("{}", one.0);
+        let mut right = String::new();
+        right.push_str("10000000\n");
+        right.push_str("11000000\n");
+        right.push_str("11111000\n");
+        right.push_str("00000000\n");
+        right.push_str("00000000\n");
+        right.push_str("10000000\n");
+        right.push_str("10000000\n");
+        right.push_str("11000111\n");
 
-        let x = format!("{one:?}");
-        let mut y = String::new();
-        y.push_str("10000000\n");
-        y.push_str("11000000\n");
-        y.push_str("11111000\n");
-        y.push_str("00000000\n");
-        y.push_str("00000000\n");
-        y.push_str("10000000\n");
-        y.push_str("10000000\n");
-        y.push_str("11000111\n");
+        assert_eq!(left, right);
+    }
 
-        assert_eq!(x , y);
+    #[test]
+    fn it_reads_a_bitboard_from_a_string() {
+        let mut s = String::new();
+        s.push_str("10000000");
+        s.push_str("11000000");
+        s.push_str("11111000");
+        s.push_str("00000000");
+        s.push_str("00000000");
+        s.push_str("10000000");
+        s.push_str("10000000");
+        s.push_str("11000111");
+        let bb = Bitboard::from(s);
+        let left = format!("{bb:?}");
+
+        let mut right = String::new();
+        right.push_str("10000000\n");
+        right.push_str("11000000\n");
+        right.push_str("11111000\n");
+        right.push_str("00000000\n");
+        right.push_str("00000000\n");
+        right.push_str("10000000\n");
+        right.push_str("10000000\n");
+        right.push_str("11000111\n");
+
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn it_reads_a_bitboard_from_a_string_and_ignores_other_chars() {
+        let mut s = String::new();
+        s.push_str("a 100  00000\n");
+        s.push_str("  110  00000xxxx");
+        s.push_str("  111  11000\n\n");
+        s.push_str("  000cc00000   ");
+        s.push_str("  000  00000  yyy");
+        s.push_str("b 100  00000---");
+        s.push_str("  100  000002222");
+        s.push_str("  110  00111 hello world1111");
+        let bb = Bitboard::from(s);
+        let left = format!("{bb:?}");
+
+        let mut right = String::new();
+        right.push_str("10000000\n");
+        right.push_str("11000000\n");
+        right.push_str("11111000\n");
+        right.push_str("00000000\n");
+        right.push_str("00000000\n");
+        right.push_str("10000000\n");
+        right.push_str("10000000\n");
+        right.push_str("11000111\n");
+
+        assert_eq!(left, right);
     }
 }
